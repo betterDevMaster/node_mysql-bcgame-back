e42f2cbdd9b5f914a1559dfdfb49d64e19cb5698
zhang <soft.dev.talent.0321@gmail.com>
2021-12-27 04:31:38 -0500

    [Update] structure

--- backoffice/adminList/adminList.module.js
+++
@@ -1,7 +0,0 @@
-var currModule = { name: "adminList" };
-
-angular.module('aq.' + currModule.name, [
-    'aq.' + currModule.name + '.controllers',
-    'aq.' + currModule.name + '.routes',
-    'aq.' + currModule.name + '.services'
-]);
--- backoffice/attribute/attribute.module.js
+++
@@ -1,7 +0,0 @@
-var currModule = {name: "attribute"};
-
-angular.module('aq.' + currModule.name, [
-    'aq.' + currModule.name + '.controllers', 
-    'aq.' + currModule.name + '.services',
-    'aq.' + currModule.name + '.routes'
-]);
