60ffadb5306e8a4bdd4682c6fdd03b481eccb393
zhang <soft.dev.talent.0321@gmail.com>
2022-01-04 02:45:27 -0500

    [Update] game center controller for bet game logic

--- center/game.center.controller.js
+++ center/game.center.controller.js
@@ -6,13 +6,29 @@ const authorize = require("_middleware/authorize");
 const gameCenter = require("./game.center.service");
 
 // routes
-router.get("/slot/bet/current", authorize(), getSlotBetCurrent);
+router.get("/current", authorize(), getSlotBetCurrent);
+router.get("/request", authorize(), getSlotBetRequest);
+router.get("/requestSpecial", authorize(), getSlotBetRequestSpecial);
 
 module.exports = router;
 
 function getSlotBetCurrent(req, res, next) {
   gameCenter
     .getSlotBetCurrent(req.query)
-    .then((current) => res.json(current))
+    .then((current) => res.json({ current }))
+    .catch(next);
+}
+
+function getSlotBetRequest(req, res, next) {
+  gameCenter
+    .getSlotBetRequest(req.query)
+    .then((data) => res.json({ data }))
+    .catch(next);
+}
+
+function getSlotBetRequestSpecial(req, res, next) {
+  gameCenter
+    .getSlotBetRequestSpecial(req.query)
+    .then((data) => res.json({ data }))
     .catch(next);
 }
